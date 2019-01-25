2e25dda228e52b21d5aec968cf012f8944501184
pi club <puea.pi.club@gmail.com>
2019-01-25 16:08:54 +0300

    first server code

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = reqire('express');
+const app = express()
+
+app.get('/temperature',function(req, res){
+  res.send('24 °C');
+});
+
+app.get('/humidity', function(req, res){
+  res.send('48%');
+});
+
+app.listen(3000, function(){
+  console.log('Server listening on port 3000');
+});
+
+
